b6038ef0e1d879cfe86183b8fa43025c02092b17
benny <ben482922@gmail.com>
2017-03-16 17:47:33 +0800

    [RM] Remove the MTK folder

--- MTK/10/main.c
+++
@@ -1,9 +0,0 @@
-//C.
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-	int a1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	printf ("%d + %d = %d\n", a1[1][3], a1[2][1], a1[1][3] + a1[2][1]);
-	return 0;
-}
